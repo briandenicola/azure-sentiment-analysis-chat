cd3a5fbf92b66bd4eb1cd99122c401084d94f9cb
Brian Denicola <bjd145@yahoo.com>
2019-02-19 16:36:23 -0600

    Updates

---
+++ src/cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"log"
+	"internal/chat"
+)
+
+func main() {
+	log.Print("Starting Server . . ")
+
+	port := ":8081"
+	if os.Getenv("GOPORT") != "" {
+		port = os.Getenv("GOPORT")
+	} 
+
+	c := chat.NewChatHandler()
+	c.InitHttpServer(port)
+}
